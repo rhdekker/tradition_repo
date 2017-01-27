17d11c8c9931e269068ec6bff8682715a1379b4e
marijnkoolen <marijn.koolen@gmail.com>
2017-01-27 09:33:45 +0100

    Remove DB startup/shutdown from config, moved to servletcontext listener

--- stemmarest/src/main/java/net/stemmaweb/rest/ApplicationConfig.java
+++ stemmarest/src/main/java/net/stemmaweb/rest/ApplicationConfig.java
@@ -3,13 +3,13 @@ package net.stemmaweb.rest;
 import java.util.HashSet;
 import java.util.Set;
 
-import javax.annotation.PostConstruct;
+//import javax.annotation.PostConstruct;
 import javax.ws.rs.core.Application;
 
-import net.stemmaweb.services.DatabaseService;
+//import net.stemmaweb.services.DatabaseService;
 
-import net.stemmaweb.services.GraphDatabaseServiceProvider;
-import org.neo4j.graphdb.GraphDatabaseService;
+//import net.stemmaweb.services.GraphDatabaseServiceProvider;
+//import org.neo4j.graphdb.GraphDatabaseService;
 
 /**
  * This is the main configuration and setup class.
@@ -21,7 +21,7 @@ public class ApplicationConfig extends Application {
     // Get the correct path to the database location
     //private static final String DB_ENV = System.getenv("DATABASE_HOME");
     //private static final String DB_PATH = DB_ENV == null ? "/home/tagaid/stemmarest/neo4jdb" : DB_ENV;
-    private static final String DB_PATH = "/home/tagaid/stemmarest/neo4jdb";
+    private static final String DB_PATH = "/data/tagaid/neo4jdb";
     
     @Override
     public Set<Class<?>> getClasses() {
@@ -34,31 +34,36 @@ public class ApplicationConfig extends Application {
         return s;
     }
     
+    /*
     @PostConstruct
     public void initializeApp()
     {
         // This has been moved to ApplicationContextListener so that 
         // apache tomcat properly shuts down neo4j.
-        
         // Connect to the database, create the root node if necessary, and leave.
-        //GraphDatabaseService db = new GraphDatabaseServiceProvider(DB_PATH).getDatabase();
-        //DatabaseService.createRootNode(db);
-        //registerShutdownHook(db);
-    }
-
+        GraphDatabaseService db = new GraphDatabaseServiceProvider(DB_PATH).getDatabase();
+        DatabaseService.createRootNode(db);
+        registerShutdownHook(db);
+    } 
+    */
+    
+    /*
     private static void registerShutdownHook( final GraphDatabaseService graphDb )
     {
         // Registers a shutdown hook for the Neo4j instance so that it
         // shuts down nicely when the VM exits (even if you "Ctrl-C" the
         // running application).
+        System.out.println("Tagaid - Stemmarest application - Registering shutdown hook from application config.");
+                
         Runtime.getRuntime().addShutdownHook( new Thread()
         {
             @Override
             public void run()
             {
+                System.out.println("Tagaid - Stemmarest application - Shutting down database from config shutdown hook.");
                 graphDb.shutdown();
-            }
+            }   
         } );
     }
-
+    */
 }
